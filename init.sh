566bf9140194d20af655588923c5cc8dce031364
12sws12 <49154405+12sws12@users.noreply.github.com>
2020-11-20 22:01:55 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -14,5 +14,19 @@ echo 'alias d="docker"' >>~/.bashrc
 echo 'alias dps="docker ps -a"' >>~/.bashrc
 echo 'alias dex="docker exec -it"' >>~/.bashrc
 
+alias kgp="kubectl get pod -o wide"
+alias kd="kubectl describe"
+alias kgd="kubectl get deploy -o wide"
+alias kgrs="kubectl get rs -o wide"
+alias kgsvc="kubectl get svc -o wide"
+alias kl="kubectl logs"
+alias ka="kubectl apply -f"
+alias kex="kubectl exec -it"
+
+alias n="nano -S -c -$"
+alias d="docker"
+alias dps="docker ps -a"
+alias dex="docker exec -it"
+
 echo 'export HISTTIMEFORMAT="%d/%m/%y %T "' >> ~/.bash_profile
 
