af00134bac465e7e72fab0686f7c8b4d1b066721
12sws12 <49154405+12sws12@users.noreply.github.com>
2020-11-20 21:37:21 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo 'alias kgp="kubectl get pod -o wide"' >>~/.bashrc
+echo 'alias kd="kubectl describe"' >>~/.bashrc
+echo 'alias kgd="kubectl get deploy -o wide"' >>~/.bashrc
+echo 'alias kgrs="kubectl get rs -o wide"' >>~/.bashrc
+echo 'alias kgsvc="kubectl get svc -o wide"' >>~/.bashrc
+echo 'alias kl="kubectl logs"' >>~/.bashrc
+echo 'alias ka="kubectl apply -f"' >>~/.bashrc
+echo 'alias kex="kubectl exec -it"' >>~/.bashrc
+
+echo 'alias n="nano -S -l -$"' >>~/.bashrc
+echo 'alias d="docker"' >>~/.bashrc
+echo 'alias dps="docker ps -a"' >>~/.bashrc
+echo 'alias dex="docker exec -it"' >>~/.bashrc
+
+echo 'export HISTTIMEFORMAT="%d/%m/%y %T "' >> ~/.bash_profile
+
